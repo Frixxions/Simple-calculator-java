3c65eb7366fb9cc5501d0de37318858a6a6b6d12
Frixxions <omerberatcengiz4@gmail.com>
2026-02-21 16:13:39 +0300

    Added while and exit.

--- Calculator.java
+++ Calculator.java
@@ -3,16 +3,22 @@ import java.util.Scanner;
 public class Calculator {
 
     public static void main(String[] args) {
+
     String girText1 = "Enter The First Number: ";
     String girText2 = "Enter The Second Number: ";
         Scanner Ulas = new Scanner(System.in);
+
+        while(true) {
+
         System.out.println("Frixxions Simple Calculator ~~~");
         System.out.println("Choose The Operation");
+        System.out.println("[0] Exit");
         System.out.println("[1] +");
         System.out.println("[2] -");
         System.out.println("[3] x");
         System.out.println("[4] ÷");
         System.out.print("Enter Number: ");
+       
 
         int Sec = Ulas.nextInt();
         if(Sec == 1) {
@@ -22,6 +28,7 @@ public class Calculator {
             int tGir2 = Ulas.nextInt();
             int T = tGir1 + tGir2;
             System.out.println("Answer: " + T);
+            
         }
         else if(Sec == 2){
             System.out.print(girText1);
@@ -31,6 +38,8 @@ public class Calculator {
             int CK = ckGir1 - ckGir2;
             System.out.println("Answer: " + CK);
 
+            
+
         }
 
         else if(Sec == 3) {
@@ -38,8 +47,9 @@ public class Calculator {
             int crGir1 = Ulas.nextInt();
             System.out.print(girText2);
             int crGir2= Ulas.nextInt();
-            int CR = crGir1 + crGir2;
-            System.out.println("Answer; " + CR);
+            int CR = crGir1 * crGir2;
+            System.out.println("Answer: " + CR);
+            
 
         }
         else if(Sec == 4) {
@@ -47,10 +57,15 @@ public class Calculator {
         int blGir1 = Ulas.nextInt();
         System.out.print(girText2);
         int blGir2 = Ulas.nextInt();
-        int BL = blGir1 / blGir2;
+        double BL = blGir1 / blGir2;
+ 
         System.out.println("Answer: " + BL);
-        }
 
-            
-        }
+        
+       }
+       else if(Sec == 0) {
+        break;
+       }
     }
+    }
+}
